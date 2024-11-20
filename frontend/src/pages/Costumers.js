43f36e289ef5b8b553b9505a50bc111016c25fe5
Maria Luiza <marialuiza.bf.silva@gmail.com>
2024-11-20 19:21:21 -0300

    criação da pagina clientes

--- frontend/src/pages/Costumers.js
+++ frontend/src/pages/Costumers.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Costumers.css'
+
+import { useState } from 'react';
 
 function Costumers() {
+  const [costumers, setCostumers] = useState([]);
+ 
+  
+  
+  
   return (
-    <div>
-      
+    <div className='container-list'>
+      <h1>Lista de Clientes</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Número de Segurança Social</th>
+            <th>Data de Nascimento</th>
+            <th>Telefone</th>
+            <th>Email</th>
+            <th>Data de Registro</th>
+            <th>Data do Último Serviço</th>
+          </tr>
+        </thead>
+        <tbody>
+          {costumers.map((costumers) => 
+          <tr key={costumers.id}>
+          <td>{costumers.id}</td>
+          <td>{costumers.name}</td>
+          <td>{costumers.socialSecurityNumber}</td>
+          <td>{new Date(costumers.dateofBirth).toLocaleDateString()}</td>
+          <td>{costumers.address}</td>
+          <td>{costumers.phone}</td>
+          <td>{costumers.email}</td>
+          <td>{new Date(costumers.dateofRegistration).toLocaleDateString()}</td>
+          <td>{new Date(costumers.dateofLastService).toLocaleDateString()}</td>
+          </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   )
 }
