859be8ef728ee8bd05c6dbb5d7531e2a14bc0953
Liliswep <liliam.silvateixeira@gmail.com>
2024-11-20 21:40:01 -0300

    home css

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,28 @@
 import Menu from '../Components/Menu/Menu';
 import React from 'react'
+import styles from '../styles/Home.css'
 
 function Home() {
   return (
-    <div>
-     
+    <div className='container-home'>
+      <h1> Bem vindo ao <span>Auto Service</span> </h1>
+        <p>Comece a gerenciar seu atendimento!</p>
+
+      <div className='item'></div>
+        <h2> Clientes </h2>
+        <p>Gerencie o atendimento com seus clientes</p>
+        <a className='vizualizar' href='/'>Vizualizar</a>
+
+        <div className='item'></div>
+        <h2> Funcionários </h2>
+        <p>Tenha controle sobre seus Funcionários</p>
+        <a className='vizualizar' href='/'>Vizualizar</a>
+
+        <div className='item'></div>
+        <h2> Veículos </h2>
+        <p>Tenha controle dos seus clientes</p>
+        <a className='vizualizar' href='/'>Vizualizar</a>
+
     </div>
   )
 }
