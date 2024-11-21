4bac371bfc19cca9a5a43fbde57d446fdfb720b0
Liliswep <liliam.silvateixeira@gmail.com>
2024-11-21 13:42:43 -0300

    home css

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,23 +6,24 @@ function Home() {
   return (
     <div className='container-home'>
         <h1 className='title'> Bem vindo ao <span>Auto Service</span></h1>
-          <p className='subtitle'>Comece a gerenciar seu atendimento!</p>
+          <p className='subtitle'>Comece a gerenciar seu atendimento!</p> 
+          <br></br>
 
       <div className='items-container'>   
       <div className='item'>
         <h2> Clientes </h2>
-        <p>Gerencie o atendimento com seus clientes</p>
+        <p>Gerencie o atendimento com seus clientes</p> <br></br>
         <a className='view-button' href='/'>Vizualizar</a>
         </div>
       <div className="item">
         <h2> Funcionários </h2>
-        <p>Tenha controle sobre seus Funcionários</p>
+        <p>Tenha controle sobre seus funcionários</p> <br></br>
         <a className='view-button' href='/'>Vizualizar</a>
       </div>
 
       <div className='item'>
         <h2> Veículos </h2>
-        <p>Tenha controle dos seus clientes</p>
+        <p>Tenha controle dos seus clientes</p> <br></br>
         <a className='view-button' href='/'>Vizualizar</a>
       </div>
       </div>
