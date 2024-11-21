b8eaded108d2501f361b834b795cbcae410830e2
Marcos Paulo <marcospaulomacedo12@gmail.com>
2024-11-20 22:28:03 -0300

    home css 2

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,22 +6,25 @@ function Home() {
   return (
     <div className='container-home'>
       <h1> Bem vindo ao <span>Auto Service</span> </h1>
-        <p>Comece a gerenciar seu atendimento!</p>
+      <p>Comece a gerenciar seu atendimento!</p>
 
-      <div className='item'></div>
+      <div className='item'>
         <h2> Clientes </h2>
         <p>Gerencie o atendimento com seus clientes</p>
         <a className='vizualizar' href='/'>Vizualizar</a>
+     </div>
 
-        <div className='item'></div>
+      <div className='item'>
         <h2> Funcionários </h2>
         <p>Tenha controle sobre seus Funcionários</p>
         <a className='vizualizar' href='/'>Vizualizar</a>
+      </div>
 
-        <div className='item'></div>
+      <div className='item'>
         <h2> Veículos </h2>
         <p>Tenha controle dos seus clientes</p>
         <a className='vizualizar' href='/'>Vizualizar</a>
+      </div>
 
     </div>
   )
