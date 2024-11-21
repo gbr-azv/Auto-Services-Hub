019a210e1cc937b7b77652ecfcc98950b013fa54
Liliswep <liliam.silvateixeira@gmail.com>
2024-11-21 10:26:14 -0300

    arrumando css

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,25 +5,26 @@ import styles from '../styles/Home.css'
 function Home() {
   return (
     <div className='container-home'>
-        <h1 className='title'> Bem vindo ao <span>Auto Service</span>
+        <h1 className='title'> Bem vindo ao <span>Auto Service</span></h1>
           <p className='subtitle'>Comece a gerenciar seu atendimento!</p>
-        </h1>
-      <div className='items-container'>
+
+      <div className='items-container'>   
+      <div className='item'>
         <h2> Clientes </h2>
         <p>Gerencie o atendimento com seus clientes</p>
         <a className='view-button' href='/'>Vizualizar</a>
-      </div>
-
-      <div className='items-container'>
+        </div>
+      <div className="item">
         <h2> Funcionários </h2>
         <p>Tenha controle sobre seus Funcionários</p>
         <a className='view-button' href='/'>Vizualizar</a>
       </div>
 
-      <div className='items-container'>
+      <div className='item'>
         <h2> Veículos </h2>
         <p>Tenha controle dos seus clientes</p>
         <a className='view-button' href='/'>Vizualizar</a>
+      </div>
       </div>
     </div>
   )
