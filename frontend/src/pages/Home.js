bed6a9b61a068c8ffc7296746ee0e26de67bfdac
Liliswep <liliam.silvateixeira@gmail.com>
2024-11-21 10:03:25 -0300

    mudança de cor

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,28 +4,27 @@ import styles from '../styles/Home.css'
 
 function Home() {
   return (
-   <div className='container-home'>
-      <h1> Bem vindo ao <span>Auto Service</span> 
-        <p>Comece a gerenciar seu atendimento!</p>
+    <div className='container-home'>
+        <h1 className='title'> Bem vindo ao <span>Auto Service</span>
+          <p className='subtitle'>Comece a gerenciar seu atendimento!</p>
         </h1>
-
-      <div className='item'>
+      <div className='items-container'>
         <h2> Clientes </h2>
         <p>Gerencie o atendimento com seus clientes</p>
-        <a className='vizualizar' href='/'>Vizualizar</a>
+        <a className='view-button' href='/'>Vizualizar</a>
       </div>
 
-        <div className='item'>
+      <div className='items-container'>
         <h2> Funcionários </h2>
         <p>Tenha controle sobre seus Funcionários</p>
-        <a className='vizualizar' href='/'>Vizualizar</a>
-        </div>
+        <a className='view-button' href='/'>Vizualizar</a>
+      </div>
 
-        <div className='item'>
+      <div className='items-container'>
         <h2> Veículos </h2>
         <p>Tenha controle dos seus clientes</p>
-        <a className='vizualizar' href='/'>Vizualizar</a>
-        </div>
+        <a className='view-button' href='/'>Vizualizar</a>
+      </div>
     </div>
   )
 }
